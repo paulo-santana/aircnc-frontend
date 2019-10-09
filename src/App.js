13886340fb309a69ca05daf5b66a85408e09e2e8
Paulo Santana <paulo.santana.r@gmail.com>
2019-10-09 15:10:50 -0300

    finish frontend application flow

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
-import api from "./services/api";
+import Routes from "./routes";
 
 import "./App.css";
 
 import logo from "./assets/logo.svg";
 
 function App() {
-  const [email, setEmail] = useState("");
-
-  async function onSubmit(event) {
-    event.preventDefault();
-    const response = await api.post("/sessions", { email });
-    console.log(response);
-  }
-
   return (
     <div className="container">
       <img src={logo} alt="AirCnC" />
       <div className="content">
-        {/* prettier-ignore */}
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
-        </p>
-        <form onSubmit={onSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Seu melhor e-mail"
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
-          <button type="submit" className="btn">
-            Entrar
-          </button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
